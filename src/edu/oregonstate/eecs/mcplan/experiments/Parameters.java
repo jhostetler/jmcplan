bfd449720e8c7612e68fee7bab48a4afb9422dde
Jesse Hostetler <hostetje@eecs.orst.edu>
2013-04-28 02:27:25 +0000

    Repo fix complete?

---
+++ src/edu/oregonstate/eecs/mcplan/experiments/Parameters.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package edu.oregonstate.eecs.mcplan.experiments;
+
+/**
+ * @author jhostetler
+ *
+ */
+public abstract class Parameters implements CsvWriter
+{
+	public abstract int policy_horizon();
+	public abstract int max_time();
+}
